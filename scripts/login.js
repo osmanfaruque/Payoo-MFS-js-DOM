d464546f1b56d7eda07b3bce4a7cd101cc18aecf
osmanfaruque <faruqueosman0@gmail.com>
2025-02-27 00:09:38 +0600

    Login Button routing to Landing Page Added

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,16 @@
 // step = 1 btn-ID ke dhorsi getElementById diyee
-document.getElementById("login-btn").addEventListener("click",function() {
-    console.log("welcome");
+document.getElementById("login-btn").addEventListener("click",function(event) {
+    event.preventDefault();
+    const mobileNumber = document.getElementById("mobile-number").value;
+    const pin = document.getElementById("pin").value;
+    if (mobileNumber.startsWith('0') && mobileNumber.length === 11) {
+        if(pin.length === 4){
+            window.location.href = "main.html";
+        }
+        else{
+            alert("Please enter a valid PIN");
+        }
+    } else {
+        alert("Please enter a valid mobile number");
+    }
 })
