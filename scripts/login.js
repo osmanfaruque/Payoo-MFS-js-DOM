e231443682ebd288337ae57a6457bf370f8bab05
osmanfaruque <faruqueosman0@gmail.com>
2025-02-27 08:02:49 +0600

    navbar added

--- scripts/login.js
+++ scripts/login.js
@@ -3,8 +3,9 @@ document.getElementById("login-btn").addEventListener("click",function(event) {
     event.preventDefault();
     const mobileNumber = document.getElementById("mobile-number").value;
     const pin = document.getElementById("pin").value;
-    if (mobileNumber.startsWith('0') && mobileNumber.length === 11) {
-        if(pin.length === 4){
+    const convertedPin = parseInt(pin);
+    if (mobileNumber.length === 11) {
+        if(convertedPin === 1234){
             window.location.href = "main.html";
         }
         else{
